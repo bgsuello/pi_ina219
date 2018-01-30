8c186f6df9bee04d410b13719f2e0794808f3e65
Bryan Glen Suello <bg.suello@gmail.com>
2018-01-30 12:39:28 +1100

    updated example1.py

--- example1.py
+++ example1.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 from ina219 import INA219
 from ina219 import DeviceRangeError
+from time import sleep, gmtime, strftime
 
 SHUNT_OHMS = 0.1
-
 
 def read():
     ina = INA219(SHUNT_OHMS, busnum=3)
     ina.configure()
-
-    print "Bus Voltage: %.3f V" % ina.voltage()
-    try:
-        print "Bus Current: %.3f mA" % ina.current()
-        print "Power: %.3f mW" % ina.power()
-        print "Shunt voltage: %.3f mV" % ina.shunt_voltage()
-    except DeviceRangeError as e:
+    while True:
+        voltage = ina.voltage()
+        now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        try:
+            current = ina.current()
+            power = ina.power()
+            shunt_voltage = ina.shunt_voltage()
+            print "[%s] -- Bus Voltage: %.3f V \t Bus Current: %.3f \t Power(mW): %.3f \t Shunt Voltage(mV): %.3f" % (now, voltage, current, power, shunt_voltage)
+        except DeviceRangeError as e:
         # Current out of device range with specified shunt resister
-        print e
+            print e
+        sleep(1)
 
 
 if __name__ == "__main__":
